c33d1c7f59abab52ab568924cdeb93f5faa590e1
Stefan Armbruster <stefan@armbruster-it.de>
2012-09-16 20:12:06 +0200

    adopt query to parse country list from wikipedia, updated countries list

--- scripts/FetchCountryList.groovy
+++ scripts/FetchCountryList.groovy
@@ -8,13 +8,13 @@ target(main: "fetch the country list from wikipedia and convert it to csv") {
         def slurper = new XmlSlurper()
         slurper.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false)
         def html = slurper.parse(reader)
-        def countryList = html.body.div[2].div[2].pre.toString()
+        def countryList =  html.'**'.find {it.@id=='mw-content-text'}.pre.toString()
 
         File output = new File(countriesCsv)
 
         output.withWriter { Writer writer ->
             countryList.eachLine { String line ->
-                if (!line.empty) {
+                if ((!line.empty) && (!(line =~ /null/))) {
                     writer.println(line)
                 }
             }
