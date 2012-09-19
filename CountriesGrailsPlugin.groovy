c95f050793fe2ef4e107cccaa1acc67aacc8b42c
Stefan Armbruster <stefan@armbruster-it.de>
2012-09-19 17:30:25 +0200

    bumping version number, amending meta information

--- CountriesGrailsPlugin.groovy
+++ CountriesGrailsPlugin.groovy
@@ -1,7 +1,7 @@
 
 class CountriesGrailsPlugin {
     // the plugin version
-    def version = "0.4-SNAPSHOT"
+    def version = "0.4"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
@@ -25,6 +25,23 @@ The countries plugins provides:
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/countries"
 
+    // License: one of 'APACHE', 'GPL2', 'GPL3'
+    def license = "APACHE"
+
+    // Details of company behind the plugin (if there is one)
+    //def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+
+    // Any additional developers beyond the author specified above.
+//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+
+    // Location of the plugin's issue tracker.
+    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPCOUNTRIES" ]
+
+    // Online location of the plugin's browseable source code.
+    def scm = [ url: "https://github.com/sarmbruster/grails-countries-plugin" ]
+
+
+
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before 
     }
@@ -39,8 +56,6 @@ The countries plugins provides:
 
     def doWithApplicationContext = { applicationContext ->
         // TODO Implement post initialization spring config (optional)
-
-
     }
 
     def onChange = { event ->
